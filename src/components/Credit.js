460a88d53845052014513556dcec563c765a8275
kirankkushwaha <kiran912002@gmail.com>
2024-11-17 04:33:40 +0530

    Initial commit with project files

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a 
+        > Kiran Kumari</a
+      >
+    </p>
+  </div>
+  );
+}
